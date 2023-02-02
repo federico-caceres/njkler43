092c094be0c0a6d531ff4954353e5e04d28a8331
Federico Cáceres <federicoenmanuel@gmail.com>
2023-02-01 21:48:18 -0300

    update

--- src/App.js
+++ src/App.js
@@ -5,17 +5,37 @@ import posts from './posts'
 // Modifica el componente App para implmentar la funcionalidad requerida
 
 class App extends Component {
+
+  state = {
+    postsList: posts
+  }
+
+  searchPost = (e) => {
+    this.setState({
+      postsList: posts.filter(data => {
+        return data.title.toLowerCase().includes(e.target.value.toLowerCase()) 
+      })
+    })
+  }
+
   render() {
     return (
       <div>
         <div className="filter">
-          <input type="text" placeholder="Ingresa el término de búsqueda" />
+          <input type="text" placeholder="Ingresa el término de búsqueda" onChange={this.searchPost} />
         </div>
         <ul>
-          <li>
-            <a href={posts[0].url}><img src={posts[0].image } /></a>
-            <p>{ posts[0].title }</p>
-          </li>
+            {
+              this.state.postsList.map( (el, i) => {
+                return (
+                          <li key={i}>
+                            <a  href={el.url}><img src={el.image } /></a>
+                            <p>{ el.title }</p>
+                          </li>
+                       )
+              })
+            }
+            
         </ul>
       </div>
     )
